44d36baeefb2dcf1d8cef68c893e8a23ebfc7658
Dao Xuan Son <sondx12@fpt.com>
2024-04-16 07:32:05 -0700

    fix-remove-object

--- main.go
+++ main.go
@@ -13,48 +13,49 @@ import (
 func main() {
 	ctx := context.Background()
 	useSSL := true
+	// clusterName := "sondx12-yditlfwp"
+	// endpoint := "s3-sgn09.fptcloud.com"
+	// accessKeyID := "122b6c03649fc4b36723"
+	// secretAccessKey := "FJm7hzvP63vaW4d0FOBeqMGA5HIDZSluzKieO4cx"
+	// bucketName := "m-fke-stg-cluster-audit-log"
 	endpoint := os.Getenv("ENDPOINT")
 	accessKeyID := os.Getenv("ACCESS_KEY")
 	secretAccessKey := os.Getenv("SECRET_KEY")
 	bucketName := os.Getenv("BUCKET_NAME")
+	clusterName := os.Getenv("CLUSTER_NAME")
 	// Initialize minio client object.
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
 		Secure: useSSL,
-		Region: "us-east-1",
 	})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	clusterName := os.Getenv("CLUSTER_NAME")
+
 	fileName := clusterName + ".log"
 	err = os.Remove("/etc/kubernetes/audit-log/" + fileName) // remove a log file
 	if err != nil {
 		klog.Info(err)
-		// return
 	}
-	klog.Infof("bucket: %s, prefix: %s", bucketName, clusterName)
+
 	objectCh := minioClient.ListObjects(ctx, bucketName, minio.ListObjectsOptions{
-		// Prefix:    clusterName,
-		// Recursive: true,
+		Prefix:    clusterName,
+		Recursive: true,
 	})
-	klog.Infof("%d", len(objectCh))
-	if len(objectCh) != 0 {
-		// Collect objects to be removed
-		var objectsToDelete []minio.ObjectInfo
-		for object := range objectCh {
-			klog.Infof("Ojb name: %s\n", object.Key)
-			if object.Err != nil {
-				log.Fatalln(object.Err)
-			}
-			objectsToDelete = append(objectsToDelete, object)
+
+	// Collect objects to be removed
+	var objectsToDelete []minio.ObjectInfo
+	for object := range objectCh {
+		klog.Infof("Ojb name: %s\n", object.Key)
+		if object.Err != nil {
+			log.Fatalln(object.Err)
 		}
-		klog.Infof("Remove folder log of cluster %s", clusterName)
-		for _, object := range objectsToDelete {
-			err := minioClient.RemoveObject(context.Background(), bucketName, object.Key, minio.RemoveObjectOptions{})
-			if err != nil {
-				log.Fatalln(err)
-			}
+		objectsToDelete = append(objectsToDelete, object)
+	}
+	for _, object := range objectsToDelete {
+		err := minioClient.RemoveObject(context.Background(), bucketName, object.Key, minio.RemoveObjectOptions{})
+		if err != nil {
+			log.Fatalln(err)
 		}
 	}
 	klog.Infof("Log file of cluster %s is removed", clusterName)
